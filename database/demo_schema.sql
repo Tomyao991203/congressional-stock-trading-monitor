a44941c9cca67cf762221395dfe7c44af30917e2
Jiaming Yao <jy991203@umd.edu>
2022-04-10 14:34:15 -0400

    removed demo files

--- database/demo_schema.sql
+++
@@ -1,9 +0,0 @@
-DROP TABLE IF EXISTS demo_table;
-
-CREATE TABLE demo_table (
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
-    member_name TEXT NOT NULL,
-    transaction_date TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    company TEXT NOT NULL,
-    value FLOAT NOT NULL
-);
