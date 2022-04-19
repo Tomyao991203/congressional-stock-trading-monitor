644b3d77ec6b15c07064bbceef74b35442514a23
Xue Qiu <xq001@terpmail.umd.edu>
2022-04-19 12:24:34 -0400

    Companies tab with companies sorted by number of transactions

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template, request, make_response, redirect
-from cstm.database_helpers import transaction_query
+from cstm.database_helpers import get_most_popular_companies, transaction_query
 import sqlite3
 
 application = app = Flask(__name__)
@@ -11,6 +11,13 @@ def root_dir():
     if request.method == "POST":
         return render_template('transactions.html', data=transaction_query(request), dark_mode=dark_mode)
     return render_template("transactions.html", dark_mode=dark_mode)
+
+@app.route('/companies', methods=['GET', 'POST'])
+def companies():
+    dark_mode = "False" if "dark_mode" not in request.cookies else request.cookies["dark_mode"]
+    if request.method == "POST":
+        return render_template('companies.html', data=get_most_popular_companies(request), dark_mode=dark_mode)
+    return render_template("companies.html", dark_mode=dark_mode)
 
 @app.route('/dark_mode')
 def dark_mode():
