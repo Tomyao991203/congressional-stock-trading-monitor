1dc00cd46674b2b10b4fa6f3dd1e58958645f929
kira <jakewilson0314@gmail.com>
2022-04-19 19:45:14 +0000

    created the tests, they are not currently working as playing with files is difficult

---
+++ tests/unit/test_pdf_scraping.py
@@ -0,0 +1,46 @@
+import unittest
+from database.pdf_scraping import *
+from os.path import exists
+
+
+class PdfScrapingCheckIfPdfIsMalformed(unittest.TestCase):
+    """
+    This test case checks the check_if_pdf_is_malformed() from pdf_scraping.py
+    """
+
+    def test_malformed_input(self):
+        db_malformed = check_if_pdf_is_malformed("database/2015_house_pdfs/Pelosi_Nancy_20002351.pdf")
+        self.assertTrue(db_malformed)
+
+    def test_not_malformed_input(self):
+        db = check_if_pdf_is_malformed("database/2015_house_pdfs/Pelosi_Nancy_10010857.pdf")
+        self.assertFalse(db)
+
+
+class PdfScrapingCreateCorrectTemplate(unittest.TestCase):
+    """
+    This test case checks the create_correct_template() from pdf_scraping.py
+    """
+
+    def test_correct_template(self):
+        create_correct_template("Pelosi_Nancy_10010857.pdf")
+        self.assertTrue(exists("database/pdf_tmp_template.tabula-template.json"))
+
+
+class PdfScrapingPdfDiscriminator(unittest.TestCase):
+    """
+    Tests pdf_discriminator() from pdf_scraping.py
+    """
+
+    def test_malformed_input(self):
+        db = pdf_discriminator("database/2015_house_pdfs/Pelosi_Nancy_20002351.pdf")
+        self.assertTrue(db is None)
+
+    def test_working_input(self):
+        db = pdf_discriminator("database/2015_house_pdfs/Pelosi_Nancy_10010857.pdf")
+        first_line_check = db[0].columns[0].lower() == 'filer_information'
+        self.assertTrue(first_line_check)
+
+    def test_not_malformed_but_incorrect_form(self):
+        db = pdf_discriminator("2015_house_pdfs/Adams_Boyce_9106272.pdf")
+        self.assertTrue(db is None)
