3beb16c4e879181ecdf200b3ffbdbc238dc8767e
kira <jakewilson0314@gmail.com>
2022-05-10 18:54:36 +0000

    fixed some capitalization stuff again and regened the csv for testing with the cap

--- tests/unit/test_process_pandas_dataframe.py
+++ tests/unit/test_process_pandas_dataframe.py
@@ -15,3 +15,4 @@ class ProcessPandasDataframeFindStart(unittest.TestCase):
         df = pdf_discriminator("database/2015_house_pdfs/Pelosi_Nancy_10010857.pdf")
         find_start_result = find_start(df)
         self.assertTrue(find_start_result is not None)
+        
